5ef74c3e4dcd2b79c09d44428c686e7afdc2e290
KS LIM <potatoscript@yahoo.com>
2021-09-01 22:49:48 +0900

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CostNag.Models;
+using CostNag.Helper;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CostNag.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        CostAPI _api = new CostAPI();
+         
+        public async Task<IActionResult> Index()
+        {
+            List<Cost> cost = new List<Cost>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/data/get-all-costs");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                cost = JsonConvert.DeserializeObject<List<Cost>>(result);
+            }
+            return View(cost);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Helper/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CostNag.Helper
+{
+    public class CostAPI
+    {
+        public HttpClient Initial()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:58810");
+            return Client;
+        }
+    }
+}
